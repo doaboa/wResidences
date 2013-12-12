2dd5a1660f386ace2c76929c2bf9f8b62311cbab
Doa <doajafri@gmail.com>
2013-12-12 07:02:22 -0600

    set up all pages, no individual tweaks

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -17,36 +17,56 @@ function sizeContent() {
 
 
 $(document).ready(function($) {
-    // $('#web').hover(
-    // 	function () {
-    // 		$('.text').animate({opacity:'0'});
-    //         $("#web").css("width", "68.5%");
-    //         $("#webuild").hide();
-    //         $("#purpose").hide();
 
-    // 	},
-    // 	function () {
-    // 		$('.text').animate({opacity:'1'});
-    //         $("#web").css("width", "8.5%");
-    //         $("#webuild").show();
-    //         $("#purpose").show();
-
-    // 	}
-    // );
+    $(".all").hide();
+    $("#slide1").show();
+    $("#arrow").click(function() {
+        $("#slide1").hide();
+        $("#slide2").show();
+        $("#arrow").click(function() {
+            $("#slide2").hide();
+            $("#slide3").show();
+            $("#arrow").click(function() {
+                $("#slide3").hide();
+                $("#slide4").show();
+                $("#arrow").click(function() {
+                    $("#slide4").hide();
+                    $("#slide5").show();
+                    $("#arrow").click(function() {
+                        $("#slide5").hide();
+                        $("#slide6").show();
+                        $("#arrow").click(function() {
+                            $("#slide6").hide();
+                            $("#slide7").show();
+                            $("#arrow").click(function() {
+                                $("#slide7").hide();
+                                $("#slide8").show();
+                                $("#arrow").click(function() {
+                                    $("#slide8").hide();
+                                    $('.text2').animate({opacity:'0'});
+                                    $("#mobile").css("width", "85.5%");
+                                    $("#webuild").hide();
+                                    $("#finaltext").show();
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 
     $('#mobile').hover(
     	function () {
-    		$('.text2').animate({opacity:'0'}, {bottom:"+=25"});
+    		$('.text2').animate({opacity:'0'});
             $("#mobile").css("width", "85.5%");
             $("#webuild").hide();
-            $("#purpose").hide();
 
     	},
     	function () {
     		$('.text2').animate({opacity:'1'}, {bottom:"-=25"});
             $("#mobile").css("width", "3.5%");
             $("#webuild").show();
-            $("#purpose").show();
     	}
     );
 
